dbec8cfdfb7998066cde956ea29a26d88336781d
occasionales <thorirmar1@gmail.com>
2018-07-19 13:58:40 +0000

    Improvements
    Adding a timer and so that the button works in changing the image

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Image, Dimensions, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+const timer = require('react-native-timer');
 
 export default class App extends React.Component {
+  constructor(){
+  super();
+  this.state={
+    imageURL : 'https://pm1.narvii.com/6079/653d4ded88f5b358bb252a771c0d1c8e4215288c_hq.jpg'
+  }
+}
+Load_New_Image=()=>{
+  this.setState({
+    imageURL : 'https://media0.giphy.com/media/vX9WcCiWwUF7G/giphy.gif'
+  },  () => timer.setTimeout(this, 'hideImg', () => this.setState({
+    imageURL : 'https://pm1.narvii.com/6079/653d4ded88f5b358bb252a771c0d1c8e4215288c_hq.jpg'
+  }), 2000) )
+}
   render() {
     return (
       <View style={styles.container}>
-        <Image source={{ uri: 'https://pm1.narvii.com/6079/653d4ded88f5b358bb252a771c0d1c8e4215288c_hq.jpg' }} style={styles.shameImage} />
-        <TouchableOpacity style={styles.shameButton}>
+        <Image source={{ uri: this.state.imageURL }} style={styles.shameImage} />
+        <TouchableOpacity onPress={this.Load_New_Image} style={styles.shameButton}>
           <Text style={styles.shameButtonText}>SHAME</Text>
         </TouchableOpacity>
       </View>
