26516872722afd69bd7e758f104c9bb9c4a1176f
Svanlaug Ingolfsdottir <svanlaug.ingolfsdottir@ticketmaster.co.uk>
2018-07-21 23:39:17 +0100

    Refactoring function names, using prettier

--- App.js
+++ App.js
@@ -1,34 +1,62 @@
-import React from 'react';
-import { Image, Dimensions, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
-const timer = require('react-native-timer');
+import React from 'react'
+import {
+  Image,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
+const timer = require('react-native-timer')
 
 export default class App extends React.Component {
-  constructor(){
-  super();
-  this.state={
-    imageURL : 'https://pm1.narvii.com/6079/653d4ded88f5b358bb252a771c0d1c8e4215288c_hq.jpg'
+  state = {
+    imageURL: shameAssets.image,
   }
-}
-Load_New_Image=()=>{
-  this.setState({
-    imageURL : 'https://media0.giphy.com/media/vX9WcCiWwUF7G/giphy.gif'
-  },  () => timer.setTimeout(this, 'hideImg', () => this.setState({
-    imageURL : 'https://pm1.narvii.com/6079/653d4ded88f5b358bb252a771c0d1c8e4215288c_hq.jpg'
-  }), 3500) )
-}
+
+  onPressShameButton() {
+    this.setState(
+      {
+        imageURL: shameAssets.gif,
+      },
+      () =>
+        timer.setTimeout(
+          this,
+          'hideImg',
+          () =>
+            this.setState({
+              imageURL: shameAssets.image,
+            }),
+          3500
+        )
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Image source={{ uri: this.state.imageURL }} style={styles.shameImage} />
-        <TouchableOpacity onPress={this.Load_New_Image} style={styles.shameButton}>
+        <Image
+          source={{ uri: this.state.imageURL }}
+          style={styles.shameImage}
+        />
+        <TouchableOpacity
+          onPress={() => this.onPressShameButton()}
+          style={styles.shameButton}
+        >
           <Text style={styles.shameButtonText}>SHAME</Text>
         </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
 
-let { width } = Dimensions.get('window');
+const shameAssets = {
+  image:
+    'https://pm1.narvii.com/6079/653d4ded88f5b358bb252a771c0d1c8e4215288c_hq.jpg',
+  gif: 'https://media0.giphy.com/media/vX9WcCiWwUF7G/giphy.gif',
+}
+
+let { width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
@@ -38,7 +66,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   shameImage: {
-    height: (width / 1.5) - 32,
+    height: width / 1.5 - 32,
     width: width - 32,
     marginBottom: 32,
     borderRadius: 80,
@@ -51,11 +79,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderColor: 'rgba(255, 255, 0, 0.5)',
-    borderWidth: 3
+    borderWidth: 3,
   },
   shameButtonText: {
     fontSize: 32,
     fontWeight: '800',
-    color: 'rgba(255, 255, 0, 0.8)'
-  }
-});
+    color: 'rgba(255, 255, 0, 0.8)',
+  },
+})
